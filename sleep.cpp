b4ae7ed95fb35be774c76b51732b3832b29a90e0
Quin <quin@asro.cc>
2025-11-03 12:23:39 +0800

    materials from canvas

---
+++ sleep.cpp
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+#include <unistd.h>
+#include<stdlib.h>
+#include<time.h>
+ 
+int main()
+{
+    int a;
+    a=10;
+    printf("hello");
+    sleep(a);        /* number of seconds for sleeping*/
+    printf("world");
+    return 0;
+}
